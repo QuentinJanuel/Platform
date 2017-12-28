4ee19b4bec260ed3078ac1ea6ecc36ba179a4150
QuentinJanuel <facemashldv@laposte.net>
2017-12-28 18:38:26 +0100

    Coucou ya un lvl editor maggel

--- level.c
+++ level.c
@@ -2,11 +2,19 @@
 
 void makeLevel(Blocks *b){
 	newBlock(b, 32, 544, 736, 32);
-	newBlock(b, 64, 480, 32, 64);
-	newBlock(b, 96, 448, 32, 96);
-	newBlock(b, 256, 480, 32, 64);
-	newBlock(b, 288, 448, 32, 96);
-	newBlock(b, 320, 512, 32, 32);
-	newBlock(b, 416, 480, 96, 32);
-	newBlock(b, 576, 96, 32, 480);
+	newBlock(b, 59, 257, 54, 296);
+	newBlock(b, 382, 444, 74, 124);
+	newBlock(b, 90, 395, 64, 35);
+	newBlock(b, 147, 409, 35, 29);
+	newBlock(b, 169, 424, 19, 16);
+	newBlock(b, 472, 430, 179, 82);
+	newBlock(b, 635, 491, 54, 53);
+	newBlock(b, 671, 130, 46, 387);
+	newBlock(b, 579, 223, 111, 55);
+	newBlock(b, 527, 353, 161, 13);
+	newBlock(b, 203, 108, 260, 181);
+	newBlock(b, 303, 402, 61, 75);
+	newBlock(b, 178, 527, 85, 34);
+	newBlock(b, 190, 507, 67, 41);
+	newBlock(b, 208, 474, 49, 81);
 }
--- main.c
+++ main.c
@@ -6,12 +6,15 @@
 #include "level.h"
 
 int main(int argc, char *argv[]){
-	Window *win = createWindow(800, 608, "Platform game", 50);
+	Window *win = createWindow(800, 608, "Platform game", 70);
 	Keyboard key = {false};
+	int imgBackground = loadImage(win, "images/background.png");
 
 	Player p1 = newPlayer(win, 150, 500);
 	Blocks blocks = initBlocks(1000);
 	makeLevel(&blocks);
+
+	int x1 = 0, y1 = 0, i, j;
 	
 	bool close = false;
 	SDL_Event e;
@@ -19,6 +22,31 @@ int main(int argc, char *argv[]){
 		while(SDL_PollEvent(&e)){
 			if(e.type == SDL_QUIT || (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE))
 				close = true;
+			else if(e.type == SDL_MOUSEBUTTONDOWN){
+				if(e.button.button == SDL_BUTTON_LEFT){
+					x1 = e.button.x;
+					y1 = e.button.y;
+				}
+			}else if(e.type == SDL_MOUSEBUTTONUP){
+				switch(e.button.button){
+					case SDL_BUTTON_LEFT:
+						newBlock(&blocks,	(e.button.x > x1) ? x1 : e.button.x,
+											(e.button.y > y1) ? y1 : e.button.y,
+											(e.button.x > x1) ? e.button.x-x1 : x1-e.button.x,
+											(e.button.y > y1) ? e.button.y-y1 : y1-e.button.y);
+						break;
+					case SDL_BUTTON_RIGHT:
+						for(i = 0; i < blocks.number; i++){
+							if(e.button.x > blocks.b[i].x && e.button.y > blocks.b[i].y &&
+							e.button.x < blocks.b[i].x+blocks.b[i].width && e.button.y < blocks.b[i].y+blocks.b[i].height){
+								for(j = i; j < blocks.number-1; j++)
+									blocks.b[j] = blocks.b[j+1];
+								blocks.number--;
+							}
+						}
+						break;
+				}
+			}
 			key.preUp = key.up;
 			getKey(&key.up, e, SDLK_UP);
 			getKey(&key.left, e, SDLK_LEFT);
@@ -26,8 +54,7 @@ int main(int argc, char *argv[]){
 		}
 		while(SDL_GetTicks()-win->pTime < 1000.0/win->FPS);
 		screenClear(win);
-		setColor(win, 0xAAA);
-		drawRect(win, 0, 0, win->width, win->height);
+		drawImage(win, imgBackground, 0, 0);
 		//
 		updatePlayer(key, &p1, blocks);
 		drawAllBlocks(win, blocks);
@@ -36,6 +63,14 @@ int main(int argc, char *argv[]){
 		SDL_Flip(win->screen);
 		win->pTime = SDL_GetTicks();
 	}
+	for(i = 0; i < blocks.number; i++){
+		fprintf(stdout, "newBlock(b, %d, %d, %d, %d);\n",
+			blocks.b[i].x,
+			blocks.b[i].y,
+			blocks.b[i].width,
+			blocks.b[i].height);
+	}
+	free(blocks.b);
 	endSDL(win);
 	return EXIT_SUCCESS;
 }
